24d83ad2a6baefb343193a1c4a89edd5eb106ccd
jpdhernandez <jpdhernandez@gmail.com>
2017-05-22 20:00:48 -0400

    Add content to recipe components and output list of recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model'
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a recipe', 'http://soupofsuccess.com/wp-content/uploads/2011/07/French-Cranberry-Chicken-Recipe.jpg'),
+    new Recipe('A Test Recipe', 'This is a recipe', 'http://soupofsuccess.com/wp-content/uploads/2011/07/French-Cranberry-Chicken-Recipe.jpg')
+  ];
 
   constructor() { }
 
